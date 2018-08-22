21094a95e271655001ee753c065ee0f9bdf63b9a
Lionel Henry <lionel.hry@gmail.com>
2018-08-22 19:38:02 +0200

    Add print method for external objects

--- R/external.R
+++ R/external.R
@@ -16,3 +16,38 @@ new_external <- function(ext) {
 is_external <- function(x) {
   inherits(x, "external")
 }
+
+#' @export
+print.external <- function(x, ...) {
+  size <- format(pryr::object_size(x), ...)
+  cat(sprintf("<external> %s\n", size))
+
+  env <- fn_env(x)
+  nms <- ls(env)
+  for (nm in nms) {
+    size <- format(pryr::object_size(env[[nm]]), ...)
+    cat(sprintf("* `%s`: %s\n", nm, size))
+  }
+
+  # Print without environment tag
+  fn <- unclass(x)
+  environment(fn) <- global_env()
+  print(unclass(fn), ...)
+
+  invisible(x)
+}
+
+# From pryr
+format.bytes <- function(x, digits = 3, ...) {
+  power <- min(floor(log(abs(x), 1000)), 4)
+  if (power < 1) {
+    unit <- "B"
+  } else {
+    unit <- c("kB", "MB", "GB", "TB")[[power]]
+    x <- x / (1000 ^ power)
+  }
+
+  x <- signif(x, digits = digits)
+  fmt <- format(unclass(x), big.mark = ",", scientific = FALSE)
+  paste(fmt, unit)
+}
